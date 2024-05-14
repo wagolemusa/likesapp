8e153fa2e9c044f261d6fa283e06e5bca239f358
wagolemusa <homiemusa@gmail.com>
2024-05-14 14:32:01 +0300

    update url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -60,6 +60,7 @@ export default async function auth(req, res) {
         signIn: "/login",
       },
       secret: process.env.NEXTAUTH_SECRET,
+      site: process.env.ENVIRONMENT_URL,
     });
   }
 
