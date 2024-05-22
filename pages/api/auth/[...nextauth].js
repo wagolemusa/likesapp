6f3b7ba5a02797a88dc9ef66890c9f00e04e7f2e
Your Name <you@example.com>
2024-05-22 07:26:08 +0300

    Add url 5

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -60,10 +60,17 @@ export default async function auth(req, res) {
   
           return session;
         },
-        async redirect(url, baseUrl) {
-        //  "https://master.d28j0wql6qmeva.amplifyapp.com"
-          return baseUrl;
-        },
+        // async redirect(url, baseUrl) {
+        //   return Promise.resolve(url)
+        //   // return baseUrl;
+        // },
+        async redirect({ url, baseUrl }) {
+          // Allows relative callback URLs
+          if (url.startsWith("/")) return `${baseUrl}${url}`
+          // Allows callback URLs on the same origin
+          else if (new URL(url).origin === baseUrl) return url
+          return baseUrl
+        }
       },
       pages: {
         signIn: "/login",
