c404636f9da8928f015031cedf0b0fa0c5a05674
Your Name <you@example.com>
2024-05-24 22:23:53 +0300

    change auth 8

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,211 +5,135 @@ import bcrypt from "bcryptjs";
 import dbConnect from "../../../backend/config/dbConnect"
 
 
-export default async function auth(req, res) {
-  return await NextAuth(req, res, {
-    session: {
-      strategy: 'jwt',
-    },
-    providers: [
-      CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          email: { label: "Email", type: "email", placeholder: "email@example.com" },
-          password: { label: "Password", type: "password" }
-        },
-        async authorize(credentials, req) {
-          await dbConnect();
-
-          const { email, password } = credentials;
-          const user = await User.findOne({ email }).select("+password");
-
-          if (!user) {
-            throw new Error("Invalid Email or Password");
-          }
-
-          const isPasswordMatched = await bcrypt.compare(password, user.password);
-
-          if (!isPasswordMatched) {
-            throw new Error("Invalid Email or Password");
-          }
-
-          return user;
-        },
-      }),
-    ],
-    callbacks: {
-      async jwt({ token, user }) {
-        if (user) {
-          token.user = user;
-        }
-
-        console.log("JWT Callback: ", token);
-        return token;
-      },
-      async session({ session, token }) {
-        session.user = token.user;
-        delete session?.user?.password;
-
-        console.log("Session Callback: ", session);
-        return session;
-      },
-      async redirect({ url, baseUrl }) {
-        const base = process.env.NEXTAUTH_URL || baseUrl;
-        console.log(`Redirecting to: ${url}, Base URL: ${base}`);
-        if (url.startsWith("/")) return `${base}${url}`;
-        else if (new URL(url).origin === base) return url;
-        return base;
-      },
-    },
-    pages: {
-      signIn: '/login',
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-    baseUrl: process.env.NEXTAUTH_URL
-  });
-}
-
 // export default async function auth(req, res) {
-//     return await NextAuth(req, res, {
-//       session: {
-//         strategy: "jwt",
-//       },
-//       providers: [
-//         CredentialsProvider({
-
-//           credentials: {
-//             // Update callbackUrl here
-//             callbackUrl: "https://master.d28j0wql6qmeva.amplifyapp.com",
+//   return await NextAuth(req, res, {
+//     session: {
+//       strategy: 'jwt',
+//     },
+//     providers: [
+//       CredentialsProvider({
+//         name: 'Credentials',
+//         credentials: {
+//           email: { label: "Email", type: "email", placeholder: "email@example.com" },
+//           password: { label: "Password", type: "password" }
 //         },
+//         async authorize(credentials, req) {
+//           await dbConnect();
 
-//           async authorize(credentials, req) {
-//             dbConnect();
-  
-//             const { email, password } = credentials;
-//             const user = await User.findOne({ email }).select("+password");
-//             if (!user) {
-//               throw new Error("Invalid Email or Password");
-//             }
-//             const isPasswordMatched = await bcrypt.compare(
-//               password,
-//               user.password
-//             );
-  
-//             if (!isPasswordMatched) {
-//               throw new Error("Invalid Email or Password");
-//             }
-  
-//             return user;
-//           },
-//         }),
-//       ],
-//       callbacks: {
-//         jwt: async ({ token, user }) => {
-//           user && (token.user = user);
+//           const { email, password } = credentials;
+//           const user = await User.findOne({ email }).select("+password");
 
-//           if(req.url === '/api/auth/session?update'){
-//             // hit the db and return the update
-//             console.log("Updating user session...", );
-//             const updatedUser = await User.findById(token.user._id)
-//             token.user = updatedUser
+//           if (!user) {
+//             throw new Error("Invalid Email or Password");
 //           }
-  
-//           return token;
-//         },
-//         session: async ({ session, token }) => {
-//           session.user = token.user;
-  
-//           // delete password from session
-//           delete session?.user?.password;
-  
-//           return session;
-//         },
 
-//         // async redirect({ url, baseUrl }) {
-//         //   return baseUrl
-//         // },
+//           const isPasswordMatched = await bcrypt.compare(password, user.password);
 
-//         async redirect({ url, baseUrl }) {
-//           // Ensures the base URL is used correctly
-//           if (url.startsWith("/")) return `${baseUrl}${url}`;
-//           else if (new URL(url).origin === baseUrl) return url;
-//           return baseUrl;
+//           if (!isPasswordMatched) {
+//             throw new Error("Invalid Email or Password");
+//           }
+
+//           return user;
 //         },
+//       }),
+//     ],
+//     callbacks: {
+//       async jwt({ token, user }) {
+//         if (user) {
+//           token.user = user;
+//         }
 
+//         console.log("JWT Callback: ", token);
+//         return token;
 //       },
-//       pages: {
-//         signIn: "/login",
+//       async session({ session, token }) {
+//         session.user = token.user;
+//         delete session?.user?.password;
+
+//         console.log("Session Callback: ", session);
+//         return session;
 //       },
-//       secret: process.env.NEXTAUTH_SECRET,
-//       baseUrl: process.env.BASE_URL || 'https://master.d28j0wql6qmeva.amplifyapp.com',
-//     });
-//   }
+//       async redirect({ url, baseUrl }) {
+//         const base = process.env.NEXTAUTH_URL || baseUrl;
+//         console.log(`Redirecting to: ${url}, Base URL: ${base}`);
+//         if (url.startsWith("/")) return `${base}${url}`;
+//         else if (new URL(url).origin === base) return url;
+//         return base;
+//       },
+//     },
+//     pages: {
+//       signIn: '/login',
+//     },
+//     secret: process.env.NEXTAUTH_SECRET,
+//     baseUrl: process.env.NEXTAUTH_URL
+//   });
+// }
 
   
-  // export default async function auth(req, res) {
-  //   return await NextAuth(req, res, {
-  //     session: {
-  //       strategy: 'jwt',
-  //     },
-  //     providers: [
-  //       CredentialsProvider({
-  //         name: 'Credentials',
-  //       //   credentials: {
-  //       //     // Update callbackUrl here
-  //       //     callbackUrl: "https://master.d28j0wql6qmeva.amplifyapp.com",
-  //       // },
-  //         async authorize(credentials, req) {
-  //           await dbConnect();
+  export default async function auth(req, res) {
+    return await NextAuth(req, res, {
+      session: {
+        strategy: 'jwt',
+      },
+      providers: [
+        CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+          email: { label: "Email", type: "email", placeholder: "email@example.com" },
+                    password: { label: "Password", type: "password" }
+          },
+          async authorize(credentials, req) {
+            await dbConnect();
   
-  //           const { email, password } = credentials;
-  //           const user = await User.findOne({ email }).select("+password");
+            const { email, password } = credentials;
+            const user = await User.findOne({ email }).select("+password");
   
-  //           if (!user) {
-  //             throw new Error("Invalid Email or Password");
-  //           }
+            if (!user) {
+              throw new Error("Invalid Email or Password");
+            }
   
-  //           const isPasswordMatched = await bcrypt.compare(password, user.password);
+            const isPasswordMatched = await bcrypt.compare(password, user.password);
   
-  //           if (!isPasswordMatched) {
-  //             throw new Error("Invalid Email or Password");
-  //           }
+            if (!isPasswordMatched) {
+              throw new Error("Invalid Email or Password");
+            }
   
-  //           return user;
-  //         },
-  //       }),
-  //     ],
-  //     callbacks: {
-  //       async jwt({ token, user }) {
-  //         if (user) {
-  //           token.user = user;
-  //         }
+            return user;
+          },
+        }),
+      ],
+      callbacks: {
+        async jwt({ token, user }) {
+          if (user) {
+            token.user = user;
+          }
   
-  //         if (req.url === '/api/auth/session?update') {
-  //           const updatedUser = await User.findById(token.user._id);
-  //           token.user = updatedUser;
-  //         }
+          if (req.url === '/api/auth/session?update') {
+            const updatedUser = await User.findById(token.user._id);
+            token.user = updatedUser;
+          }
   
-  //         return token;
-  //       },
-  //       async session({ session, token }) {
-  //         session.user = token.user;
+          return token;
+        },
+        async session({ session, token }) {
+          session.user = token.user;
   
-  //         // delete password from session
-  //         delete session?.user?.password;
+          // delete password from session
+          delete session?.user?.password;
   
-  //         return session;
-  //       },
-  //       async redirect({ url, baseUrl }) {
-  //         if (url.startsWith("/")) return `${baseUrl}${url}`;
-  //         else if (new URL(url).origin === baseUrl) return url;
-  //         return baseUrl;
-  //       },
-  //     },
-  //     pages: {
-  //       signIn: '/login',
-  //     },
-  //     secret: process.env.NEXTAUTH_SECRET,
-  //     baseUrl: process.env.BASE_URL || 'https://master.d28j0wql6qmeva.amplifyapp.com',
-  //   });
-  // }
+          return session;
+        },
+        async redirect({ url, baseUrl }) {
+          if (url.startsWith("/")) return `${baseUrl}${url}`;
+          else if (new URL(url).origin === baseUrl) return url;
+          return baseUrl;
+        },
+      },
+      pages: {
+        signIn: '/login',
+      },
+      secret: process.env.NEXTAUTH_SECRET,
+      baseUrl: process.env.baseUrl || 'https://master.d28j0wql6qmeva.amplifyapp.com',
+    });
+  }
   
