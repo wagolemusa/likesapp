fab419bed417846008513a1120ed12cc139b2db8
Your Name <you@example.com>
2024-05-26 13:23:40 +0300

    Change url

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -124,7 +124,7 @@ import dbConnect from "../../../backend/config/dbConnect"
           return session;
         },
         async redirect({ url, baseUrl }) {
-          if (url.startsWith("https://master.d28j0wql6qmeva.amplifyapp.com")) return `${baseUrl}${url}`;
+          if (url.startsWith("h")) return `${baseUrl}${url}`;
           else if (new URL(url).origin === baseUrl) return url;
           return baseUrl;
         },
