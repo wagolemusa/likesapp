dea0b5d640af4de62e8967c413403e8229f829bf
Your Name <you@example.com>
2024-05-27 14:34:10 +0300

    Change url 7

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -53,11 +53,7 @@ export default async function auth(req, res) {
         console.log("Session Callback: ", session);
         return session;
       },
-      async redirect({ url, baseUrl }) {
-          const base = process.env.NEXTAUTH_URL || baseUrl;
-          const redirectUrl = url.startsWith('/') ? `${base}${url}` : url;
-          return redirectUrl;
-        }
+   
     },
     pages: {
         signIn: process.env.NEXTAUTH_URL + '/login',
