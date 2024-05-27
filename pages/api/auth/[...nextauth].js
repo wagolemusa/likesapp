869627b946b99b4698f40252aba1b8f69d2718c5
Your Name <you@example.com>
2024-05-27 10:12:45 +0300

    Change url 2

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -62,7 +62,6 @@ export default async function auth(req, res) {
       },
     },
     pages: {
-        signIn: process.env.NEXTAUTH_URL + 'https://master.d28j0wql6qmeva.amplifyapp.com/login' || 'https://master.d28j0wql6qmeva.amplifyapp.com',
         signOut: process.env.NEXTAUTH_URL + '/',
         error: process.env.NEXTAUTH_URL + '/thanks'
     },
